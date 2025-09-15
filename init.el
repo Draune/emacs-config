943d8953097ea1495d11ed3ff57a4211e0ee8193
Draune <sioul.duaner@gmail.com>
2025-09-15 12:10:19 +0200

    move keybindings to key.el

--- init.el
+++ init.el
@@ -42,10 +42,19 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
+;; Setup use-package from reddit https://www.reddit.com/r/emacs/g2m2rg/a_guide_to_configure_emacs (anser from magicalmurray)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile (require 'use-package))
+
 ;; Load configs
 
 ;; Utilities functions
 (load-file "~/.emacs.d/util.el")
+
+;; Key bindings
+(load-file "~/.emacs.d/key.el")
 
 ;; Here is all the configs directly linked to configuring emacs defaults
 
@@ -61,19 +70,6 @@
 
 ;; Enable global-visual-line-mode (words aren't cut in half at the end of line)
 (global-visual-line-mode)
-
-;; Customize startup showing version of Emacs and init time
-(defun emacs-ppid ()
-  "Retourne le PID (string) du processus parent d’Emacs."  
-  (string-trim
-   (shell-command-to-string
-    (format "ps -o ppid= -p %d" (emacs-pid)))))
-
-(defun emacs-parent-name ()
-  "Retourne le nom du processus parent d’Emacs (Linux uniquement)."
-  (string-trim
-   (shell-command-to-string
-    (format "ps -o comm= -p %s" (emacs-ppid)))))
 
 (defun my-scratch-init () (interactive)
        (switch-to-buffer "*scratch*")
@@ -98,10 +94,6 @@
 (icomplete-vertical-mode)
 ;; Disble the displaying of *Completions* since I use icomplete
 (setq completion-auto-help nil)
-
-;; Setup which-key (key cheatsheet that is displayed during key sequences)
-(which-key-mode)
-(which-key-setup-minibuffer)
 
 ;; Highlight the cursor line
 (global-hl-line-mode)
@@ -129,50 +121,11 @@
 (setq completion-styles '(basic substring partial-completion flex))
 (setq completion-ignore-case t)
 
-;; My keybindings
-;; Global keybindings:
-(bind-key "C-c e" 'eshell)
-(bind-key "C-c f" 'elfeed)
-(bind-key "C-c w" 'eww)
-(bind-key "C-c t" 'google-translate-buffer)
-
-;; + devil repeatable keys
-(bind-key "C-c n" 'next-buffer)
-(bind-key "C-c p" 'previous-buffer)
-(bind-key "C-c k" 'kill-current-buffer)
-;; Doublons here for better experience with Azerty keyboards 
-(bind-key "C-x \"" 'split-window-right)
-(bind-key "C-x é" 'split-window-below)
-(bind-key "C-x à" 'delete-window)
-(bind-key "C-x &" 'delete-other-windows)
-
-;; better functions fo default keybindings
-(bind-key "C-k" 'kill-whole-line)
-(bind-key "C-v" (lambda () (interactive) (next-line 10)))
-(bind-key "M-v" (lambda () (interactive) (previous-line 10)))
-(bind-key "C-o" (lambda () (interactive)
-       (call-interactively 'move-beginning-of-line)
-       (call-interactively 'open-line)
-       (call-interactively 'indent-for-tab-command)))
-(bind-key "M-a" 'beginning-of-buffer)
-(bind-key "M-e" 'end-of-buffer)
-
 ;; Local keybindings:
 ;; Here because by default TAB calls the default completion help (which I deactivated but I don't want it to be useless)
 (bind-key "<tab>" 'icomplete-fido-ret icomplete-fido-mode-map)
-;; Use M-p and M-n for the command history when using M-&
-;; Already binded but (I don't really know why) if I don't declare it
-;; there is a bug when repeated
-(bind-key "M-p" 'previous-line-or-history-element minibuffer-local-shell-command-map)
-(bind-key "M-n" 'next-line-or-history-element minibuffer-local-shell-command-map)
 
 ;; Here are all the package intallations and setups
-
-;; Setup use-package from reddit https://www.reddit.com/r/emacs/g2m2rg/a_guide_to_configure_emacs (anser from magicalmurray)
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(eval-when-compile (require 'use-package))
 
 ;; Install and use ef-themes
 (use-package ef-themes
@@ -235,41 +188,6 @@
   :init ; with this, when eshell is launched, it will be emulated in eat
   (add-hook 'eshell-load-hook #'eat-eshell-mode)
   )
-
-;; Install devil (translate "," to "C-"; and use support of which-key from fbrosda)
-(use-package devil
-  :vc (:url "https://github.com/fbrosda/devil"
-	    :branch "dev"
-	    :rev :newest)
-  :ensure t
-  :demand t
-  :custom
-  ;; Don't use keys that are usefull when repeated (for exemple ";" for comments)
-  (devil-exit-key "q")
-  (devil-all-keys-repeatable t)
-  (devil-highlight-repeatable t)
-  (devil-repeatable-keys '(("%k p" "%k n" "%k b" "%k f" "%k a" "%k e")
-			   ;; repeatable keys for window and buffer management and find-file and dired
-			   ;; I went a little crazy here but since most of the time actions on buffers
-			   ;; and windows are followed by other commands like "C-n" I believe it's ok
-			   ("%k c n" "%k c p""%k c k" "%k x o" "%k x &" "%k x é" "%k x \"" "%k x à" "%k x %k f" "%k x d" "%k x b" "%k x 0" "%k x 1" "%k x 2" "%k x 3")
-			   ;; repeatable keys for movement M- keybindings
-			   ("%k %k n" "%k %k p" "%k %k f" "%k %k b")))
-  
-  :bind
-  ([remap describe-key] . devil-describe-key)
-  :config
-  (global-devil-mode)
-  (global-set-key (kbd "C-,") 'global-devil-mode)
-  ;; I don't really know why but if I don't do this line before calling assoc-delete-all
-  ;; it will not do it. I use asoc-delete-all to delete the devil special key "%k %k" (", ,")
-  ;; so ", ," will be translated to "M-". Plus this line is usefull to mark things
-  (add-to-list 'devil-special-keys `(", , SPC" . ,(devil-key-executor "C-SPC")))
-  (assoc-delete-all "%k %k" devil-special-keys)
-  (setq devil-translations '((", , ," . "C-M-")
-			     (", ," . "M-")
-			     ("," . "C-")
-			     )))
 
 ;; Install elfeed (RSS reader ~ reader for web feeds like reddit, arxiv,...)
 (use-package elfeed :ensure t)
---
+++ key.el
@@ -0,0 +1,71 @@
+;; Install devil (translate "," to "C-"; and use support of which-key from fbrosda)
+(use-package devil
+  :vc (:url "https://github.com/fbrosda/devil"
+	    :branch "dev"
+	    :rev :newest)
+  :ensure t
+  :demand t
+  :custom
+  ;; Don't use keys that are usefull when repeated (for exemple ";" for comments)
+  (devil-exit-key "q")
+  (devil-all-keys-repeatable t)
+  (devil-highlight-repeatable t)
+  (devil-repeatable-keys '(("%k p" "%k n" "%k b" "%k f" "%k a" "%k e")
+			   ;; repeatable keys for window and buffer management and find-file and dired
+			   ;; I went a little crazy here but since most of the time actions on buffers
+			   ;; and windows are followed by other commands like "C-n" I believe it's ok
+			   ("%k c n" "%k c p""%k c k" "%k x o" "%k x &" "%k x é" "%k x \"" "%k x à" "%k x %k f" "%k x d" "%k x b" "%k x 0" "%k x 1" "%k x 2" "%k x 3")
+			   ;; repeatable keys for movement M- keybindings
+			   ("%k %k n" "%k %k p" "%k %k f" "%k %k b")))
+  
+  :bind
+  ([remap describe-key] . devil-describe-key)
+  :config
+  (global-devil-mode)
+  (global-set-key (kbd "C-,") 'global-devil-mode)
+  ;; I don't really know why but if I don't do this line before calling assoc-delete-all
+  ;; it will not do it. I use asoc-delete-all to delete the devil special key "%k %k" (", ,")
+  ;; so ", ," will be translated to "M-". Plus this line is usefull to mark things
+  (add-to-list 'devil-special-keys `(", , SPC" . ,(devil-key-executor "C-SPC")))
+  (assoc-delete-all "%k %k" devil-special-keys)
+  (setq devil-translations '((", , ," . "C-M-")
+			     (", ," . "M-")
+			     ("," . "C-")
+			     )))
+
+;; My keybindings
+;; Global keybindings:
+(bind-key "C-c e" 'eshell)
+(bind-key "C-c f" 'elfeed)
+(bind-key "C-c w" 'eww)
+(bind-key "C-c t" 'google-translate-buffer)
+
+;; + devil repeatable keys
+(bind-key "C-c n" 'next-buffer)
+(bind-key "C-c p" 'previous-buffer)
+(bind-key "C-c k" 'kill-current-buffer)
+;; Doublons here for better experience with Azerty keyboards 
+(bind-key "C-x \"" 'split-window-right)
+(bind-key "C-x é" 'split-window-below)
+(bind-key "C-x à" 'delete-window)
+(bind-key "C-x &" 'delete-other-windows)
+
+;; better functions fo default keybindings
+(bind-key "C-k" 'kill-whole-line)
+(bind-key "C-v" (lambda () (interactive) (next-line 10)))
+(bind-key "M-v" (lambda () (interactive) (previous-line 10)))
+(bind-key "C-o" (lambda () (interactive)
+       (call-interactively 'move-beginning-of-line)
+       (call-interactively 'open-line)
+       (call-interactively 'indent-for-tab-command)))
+(bind-key "M-a" 'beginning-of-buffer)
+(bind-key "M-e" 'end-of-buffer)
+;; Use M-p and M-n for the command history when using M-&
+;; Already binded but (I don't really know why) if I don't declare it
+;; there is a bug when repeated
+(bind-key "M-p" 'previous-line-or-history-element minibuffer-local-shell-command-map)
+(bind-key "M-n" 'next-line-or-history-element minibuffer-local-shell-command-map)
+
+;; Setup which-key (key cheatsheet that is displayed during key sequences)
+(which-key-mode)
+(which-key-setup-minibuffer)
