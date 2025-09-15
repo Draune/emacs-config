531da74ef005acd4b090f1e16e05cddcfad30ae5
Draune <sioul.duaner@gmail.com>
2025-09-15 12:24:43 +0200

    create auto-complete.el

--- init.el
+++ init.el
@@ -52,58 +52,14 @@
 
 ;; Utilities functions
 (load-file "~/.emacs.d/util.el")
-
 ;; Tools
 (load-file "~/.emacs.d/tools.el")
-
 ;; Better defaults
 (load-file "~/.emacs.d/defaults.el")
+;; Auto-complete (Corfu)
+(load-file "~/.emacs.d/auto-complete.el")
 ;; Key bindings
 (load-file "~/.emacs.d/key.el")
-
-;; Install corfu (autocompletion)
-(use-package corfu
-  :ensure t
-  :defer t
-  ;; Optional customizations
-  :custom
-  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
-  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-  (corfu-preview-current nil)    ;; Disable current candidate preview
-  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
-  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
-
-  ;; Enable Corfu only for certain modes. See also `global-corfu-modes'.
-  ;; :hook ((prog-mode . corfu-mode)
-  ;;        (shell-mode . corfu-mode)
-  ;;        (eshell-mode . corfu-mode))
-
-  :init
-
-  ;; Recommended: Enable Corfu globally.  Recommended since many modes provide
-  ;; Capfs and Dabbrev can be used globally (M-/).  See also the customization
-  ;; variable `global-corfu-modes' to exclude certain modes.
-  (global-corfu-mode)
-
-  ;; Enable optional extension modes:
-  (corfu-history-mode)
-  ;; (corfu-popupinfo-mode)
-  ;; Enable auto completion and configure quitting
-  (setq corfu-auto t
-	corfu-quit-no-match 'separator ;; or t
-	corfu-auto-prefix 1
-	corfu-auto-delay  0) 
-  (setq corfu-bar-width 1)
-  )
-
-;; Install corfu-terminal
-(use-package corfu-terminal
-  :ensure t
-  :config
-  (corfu-terminal-mode t)
-  (setq corfu-terminal-disable-on-gui nil)
-  )
 
 ;; Install async (for dired-async)
 (use-package async
