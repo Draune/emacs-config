a5fca43ca98f1530a8066a2317d8ce4cb2d30800
Draune <sioul.duaner@gmail.com>
2025-12-10 16:19:26 +0100

    Move auto-complete.el in prog.el

--- prog.el
+++ prog.el
@@ -15,3 +15,53 @@
   :init
   (global-lsp-bridge-mode)
   )
+
+;; Install corfu (autocompletion just in eshell and shell commands)
+(use-package corfu
+  :ensure t
+  :defer t
+  ;; Optional customizations
+  :custom
+  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  
+  :hook (
+	 ;; (prog-mode . corfu-mode)
+         (shell-mode . corfu-mode)
+         (eshell-mode . corfu-mode)
+	 )
+
+  ;; Enable Corfu only for certain modes. See also `global-corfu-modes'.
+  :init
+
+  ;; Recommended: Enable Corfu globally.  Recommended since many modes provide
+  ;; Capfs and Dabbrev can be used globally (M-/).  See also the customization
+  ;; variable `global-corfu-modes' to exclude certain modes.
+  (global-corfu-mode)
+  
+  ;; Enable optional extension modes:
+  (corfu-history-mode)
+  ;; (corfu-popupinfo-mode)
+  ;; Enable auto completion and configure quitting
+  (setq corfu-auto t
+	corfu-quit-no-match 'separator ;; or t
+	corfu-auto-prefix 1
+	corfu-auto-delay  0) 
+  (setq corfu-bar-width 1)
+
+  (add-hook 'eshell-mode-hook (lambda ()
+                              (setq-local corfu-auto nil)
+                              (corfu-mode)))
+  (setq global-corfu-minibuffer
+      (lambda ()
+        (not (or (bound-and-true-p mct--active)
+                 (bound-and-true-p vertico--input)
+                 (eq (current-local-map) read-passwd-map)))))
+  (add-hook 'prog-mode-hook
+          (lambda ()
+            (setq-local corfu-auto nil)))
+  )
