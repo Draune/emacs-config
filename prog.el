626dd86c57dc7cff007e8dc4c712fa754e342ea4
Draune <sioul.duaner@gmail.com>
2025-12-17 17:26:00 +0100

    Use corfu + lsp-mode instead of lsp-bridge

--- prog.el
+++ prog.el
@@ -1,17 +1,34 @@
-;; Install LSP with lsp-bridge
+(use-package corfu
+  ;; Optional customizations
+  ;; :custom
+  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match 'insert) ;; Configure handling of exact matches
 
-;; Dependencies
-(use-package markdown-mode
-  :ensure t)
-(use-package yasnippet
-  :ensure t
-  :config
-  (yas-global-mode 1)
-  )
+  ;; Enable Corfu only for certain modes. See also `global-corfu-modes'.
+  ;; :hook ((prog-mode . corfu-mode)
+  ;;        (shell-mode . corfu-mode)
+  ;;        (eshell-mode . corfu-mode))
 
-(use-package lsp-bridge
-  :vc (:url "https://github.com/manateelazycat/lsp-bridge")
-  :ensure t
   :init
-  (global-lsp-bridge-mode)
+
+  ;; Recommended: Enable Corfu globally.  Recommended since many modes provide
+  ;; Capfs and Dabbrev can be used globally (M-/).  See also the customization
+  ;; variable `global-corfu-modes' to exclude certain modes.
+  (global-corfu-mode)
+
+  ;; Enable optional extension modes:
+  ;; (corfu-history-mode)
+  ;; (corfu-popupinfo-mode)
+
+  :config
+  ;; Enable auto completion
+  (setq corfu-auto        t
+	corfu-auto-delay  0 
+	corfu-auto-prefix 0)
   )
+
+(use-package lsp-mode :ensure t)
