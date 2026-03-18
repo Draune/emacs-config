884c79e91530762ed6529a5707187b1e62cdc3ea
Draune <sioul.duaner@gmail.com>
2026-03-18 21:55:45 +0100

    revert from lsp-bridge

--- prog.el
+++ prog.el
@@ -8,16 +8,7 @@
   (global-corfu-mode)
   )
 
-<<<<<<< HEAD
-(use-package lsp-bridge
-  :vc (:url "https://github.com/manateelazycat/lsp-bridge.git")
-  :ensure t
-  :config
-  (global-lsp-bridge-mode)
-  )
-=======
 (use-package lsp-mode :ensure t)
->>>>>>> parent of bda9a95 (retry lsp-bridge)
 
 (use-package nix-mode
   :ensure t
