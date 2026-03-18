8a69d9206411069212a280705a9b2e2374e96db7
Draune <sioul.duaner@gmail.com>
2026-03-18 21:53:24 +0100

    Revert "retry lsp-bridge"
    This reverts commit bda9a956fd7297675dc03349180a31450d09fe46.

--- prog.el
+++ prog.el
@@ -1,19 +1,23 @@
-;; Install LSP with lsp-bridge
-;; Dependencies
-(use-package markdown-mode
-  :ensure t)
-(use-package yasnippet
+(use-package corfu
   :ensure t
   :config
-  (yas-global-mode 1)
+  ;; Enable auto completion
+  (setq corfu-auto        t
+	corfu-auto-delay  0 
+	corfu-auto-prefix 1)
+  (global-corfu-mode)
   )
 
+<<<<<<< HEAD
 (use-package lsp-bridge
   :vc (:url "https://github.com/manateelazycat/lsp-bridge.git")
   :ensure t
   :config
   (global-lsp-bridge-mode)
   )
+=======
+(use-package lsp-mode :ensure t)
+>>>>>>> parent of bda9a95 (retry lsp-bridge)
 
 (use-package nix-mode
   :ensure t
