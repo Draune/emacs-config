f40dbb8b76dad95a07db44b59e97c5f028fa78cc
Draune <sioul.duaner@gmail.com>
2026-03-21 17:09:50 +0100

    transient was in .gitignore

---
+++ packages/transient/transient-pkg.el
@@ -0,0 +1,13 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "transient" "20260320.2319"
+  "Transient commands."
+  '((emacs    "28.1")
+    (compat   "30.1")
+    (cond-let "0.2")
+    (seq      "2.24"))
+  :url "https://github.com/magit/transient"
+  :commit "adaf4e06e541745216bfabf8c4af1812f15ae7d1"
+  :revdesc "adaf4e06e541"
+  :keywords '("extensions")
+  :authors '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev")))
