bc64fa81ab5331f052010f5576d487861aae7fee
user <user@laptop>
2026-05-14 11:29:59 +0200

    forgot topspace packages in /packages

---
+++ packages/topspace/topspace-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "topspace" "20220824.134"
+  "Recenter line 1 with scrollable upper margin/padding."
+  '((emacs "25.1"))
+  :url "https://github.com/trevorpogue/topspace"
+  :commit "33c2a6f0a11d1d88cdb2065c5a897e33507f4c86"
+  :revdesc "33c2a6f0a11d"
+  :keywords '("convenience" "scrolling" "center" "cursor" "margin" "padding")
+  :authors '(("Trevor Edwin Pogue" . "trevor.pogue@gmail.com"))
+  :maintainers '(("Trevor Edwin Pogue" . "trevor.pogue@gmail.com")))
