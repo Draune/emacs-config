e33acddfef67a67c63b3f9b624383bac9e2d7629
user <user@laptop>
2026-05-14 12:17:05 +0200

    fix prog.el

--- prog.el
+++ prog.el
@@ -7,21 +7,38 @@
   ('prog-mode-hook . #'rainbow-delimiters-mode)
   )
 
-(require 'corfu)
-(setq corfu-auto        t
-      corfu-auto-delay  0 
-      corfu-auto-prefix 1)
-(global-corfu-mode)
+(use-package corfu
+  :config
+  (setq corfu-auto        t
+	corfu-auto-delay  0 
+	corfu-auto-prefix 1)
+  (global-corfu-mode)
+)
 
-(require 'lsp-mode)
+(use-package lsp-mode
+  :defer t
+  :commands
+  lsp
+  :hook
+  ('prog-mode-hook #'lsp)
+  :config
+  (setq lsp-enable-snippet nil
+	lsp-lens-enable nil
+	lsp-modeline-diagnostics-enable nil
+	lsp-modeline-code-actions-enable nil
+	lsp-modeline-workspace-status-enable nil
+	lsp-headerline-breadcrumb-enable nil)
+  )
 
-(require 'markdown-mode)
-(require 'markdown-toc)
+(use-package markdown-mode)
 
-(setq markdown-toc-preset 'pandoc)
-(setq markdown-command "pandoc --from markdown_github -t html5 --mathjax --highlight-style pygments --standalone")
-(setq markdown-toc-header-toc-title "**Sommaire**")
-
-;; Autogenerate and refresh TOC when saving a markdown file
-(add-hook 'markdown-mode-hook (lambda ()
-				(add-hook 'before-save-hook #'markdown-toc-generate-or-refresh-toc nil t)))
+(use-package markdown-toc
+  :config 
+  (setq markdown-toc-preset 'pandoc)
+  (setq markdown-command "pandoc --from markdown_github -t html5 --mathjax --highlight-style pygments --standalone")
+  (setq markdown-toc-header-toc-title "**Sommaire**")
+  :hook
+  ;; Autogenerate and refresh TOC when saving a markdown file
+  (add-hook 'markdown-mode-hook (lambda ()
+				  (add-hook 'before-save-hook #'markdown-toc-generate-or-refresh-toc nil t)))
+  )
