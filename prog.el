e156f5a6085390c76ed77c344bcb0a7e99bcc2a7
user <user@laptop>
2026-05-31 17:01:28 +0200

    add git-gutter

--- prog.el
+++ prog.el
@@ -58,3 +58,33 @@
   ('markdown-mode-hook . (lambda ()
 			   (add-hook 'before-save-hook #'markdown-toc-generate-or-refresh-toc nil t)))
   )
+
+(use-package git-gutter
+  :hook (prog-mode . git-gutter-mode)
+  :config
+  (setq git-gutter:update-interval 0.02))
+
+(use-package git-gutter-fringe
+  :config
+  (fringe-helper-define 'git-gutter-fr:added '(center repeated)
+    "XXX....."
+    "XXX....."
+    "XXX....."
+    "XXX.....")
+  (fringe-helper-define 'git-gutter-fr:modified '(center repeated)
+    "XXX....."
+    "XXX....."
+    "XXX....."
+    "XXX.....")
+  (fringe-helper-define 'git-gutter-fr:deleted 'bottom
+    "X......."
+    "XX......"
+    "XXX....."
+    "XXXX...."
+    "XXXXX..."
+    "XXXXXX.."
+    "XXXXX..."
+    "XXXX...."
+    "XXX....."
+    "XX......"
+    "X......."))
