66da1421de8f2b47cbbed3ef1aafaef3b5f890db
user <user@laptop>
2026-07-18 14:39:25 +0200

    correct build.el (pb with compat and s)

--- build.el
+++ build.el
@@ -1,18 +1,24 @@
 ;; to generate offline config run :
 ;; export EMACS_BUILD_CONFIG=1 && emacs
 (when (getenv "EMACS_BUILD_CONFIG")
+  ;; if this line is not present, emacs will not upgrade compat
+  (setq package-install-upgrade-built-in t)
+  
   (require 'package)
   
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-  
   (delete-directory "~/.emacs.d/elpa/" t)
   (delete-directory "~/.emacs.d/config-build/" t)
   (dolist (pkg-info
-	   '((consult nil)
+	   '((compat nil) 		; to install compat-31 because of
+					; problems with exwm and marginalia
+	     (consult nil)
 	     ("https://github.com/fbrosda/devil.git" "which-key-support")
 	     (exwm nil)
 	     (git-gutter-fringe nil)
 	     (languagetool nil)
+	     (s nil)			; lemon requires it but doesn't install
+					; it when using this method
 	     ("https://codeberg.org/emacs-weirdware/lemon.git" nil)
 	     (marginalia nil)
 	     (speed-type nil))
