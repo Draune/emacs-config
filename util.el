6fbcb6e43f6e89aba442826c2bcd4b527da5b072
user <user@laptop>
2026-08-14 19:37:04 +0200

    adding char-choice-menu to create a menu from a list (like org-todo)

--- util.el
+++ util.el
@@ -9,3 +9,31 @@
   (string-trim
    (shell-command-to-string
     (format "ps -o comm= -p %s" (my/emacs-ppid)))))
+
+(defun char-choice-menu (choice-list)
+  "Allow to create a choice menu from a list of choices, a choice is of the
+form: '(KEY PROMPT-NAME FUNCTION)
+
+For exemple the list of choice:
+'((\"e\" \"Exit\" (lambda () (message \"Exit\")))
+  (\"r\" \"Reboot\" (lambda () (message \"Reboot\"))))
+Will prompt : \"Choose: Exit(e) Reboot(r)\"
+Pressing \"e\" will result of the execution of (lambda () (message \"Exit\"))"
+  (let ((prompt
+	 (concat "Choose: "
+		 (let (res)
+		   (dolist (choice choice-list res)
+		     (setq res
+			   (concat res
+				   (nth 1 choice)
+				   "("
+				   (car choice)
+				   ") "))))))
+	(chars
+	 (let (res) (dolist (choice choice-list res)
+		      (setq res (cons (car choice) res))
+		      ))))
+    (let ((arg (read-char-choice prompt chars)))
+      ;; (message (char-to-string arg))
+      (funcall (nth 2 (assoc (char-to-string arg) choice-list))))
+    ))
